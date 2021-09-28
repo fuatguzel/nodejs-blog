948ef4600ec4c50fb1435b533f6cfe279a8780b9
fuatguzel <fuatguzel754@gmail.com>
2021-09-28 22:47:02 +0300

    Add pug to project

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
+app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
 })
 
 const port = 3000;
