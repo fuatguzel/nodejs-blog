f267341f88b4dc8ef4c5d180ba832bfdef6ce171
fuatguzel <fuatguzel754@gmail.com>
2021-10-08 23:25:59 +0300

    Show single article with details. Also delete post.

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const path = require('path')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+
+mongoose.connect('mongodb+srv://fuatguzel:BuLmggoAU1L4vKka@cluster0.sxzwy.mongodb.net/node-photo-gallery?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true,
+  //useCreateIndex: true
+})
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
 
+app.get('/', async (req, res) => {
+//   const articles = [{
+//     title: 'Article 1',
+//     description: 'Article Description',
+//     createdAt: new Date()
+//   },
+//   {
+//     title: 'Article 2',
+//     description: 'Article Description',
+//     createdAt: new Date()
+//   },
+// ]
 
-app.get('/', (req, res) => {
-  const articles = [{
-    title: 'Article 1',
-    description: 'Article Description',
-    createdAt: new Date()
-  },
-  {
-    title: 'Article 2',
-    description: 'Article Description',
-    createdAt: new Date()
-  },
-]
-  res.render('index', {articles: articles})
+  const articles = await Article.find({}).sort('-createdAt')
+  res.render('articles/index', {articles})
 })
 
 app.get('/about', (req, res) => {
   res.render('about')
 })
+
+app.use('/articles', articleRouter)
 
 const port = 3000;
 app.listen(port, () => {
